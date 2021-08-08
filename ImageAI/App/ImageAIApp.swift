d01e810ae08b83c8e5f639d0b875d4e7ed8b34f4
mlukacs <mlukacs@deezer.com>
2021-08-08 17:42:00 +0200

    first commit

---
+++ ImageAI/App/ImageAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAIApp.swift
+//  ImageAI
+//
+//  Created by Martin Lukacs on 08/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ImageAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
